700c4659a9f0d9e6d491fa007bbeed6100833792
Sakan <sakan.nir@gmail.com>
2024-03-25 00:06:55 +0700

    Update

--- main.py
+++ main.py
@@ -2,28 +2,14 @@
 The main script to run a web scraping of the https://www.prydwen.gg/star-rail/ website.
 This script ask users to choose how they want to scrap from the website.
 """
-import logging
+from loguru import logger
 
 from codes import web_scrap
 from codes import get_urls_auto
 
-logging.basicConfig(
-    filename='main.log',
-    level=logging.DEBUG,
-    format='%(asctime)s | %(levelname)s | %(name)s | %(filename)s | %(funcName)s | %(lineno)d | %(message)s',
-    filemode='w'
-)
-
-selenium_logger = logging.getLogger('selenium')
-selenium_logger.setLevel(logging.INFO)
-
-# Create a StreamHandler for console output
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.WARNING)
-
-# Add the console handler to the root logger
-root_logger = logging.getLogger()
-root_logger.addHandler(console_handler)
+logger.add('main.log',
+           format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {name} | {module} | {function} | {line} | {message}",
+           mode='w')
 
 
 def check_auto_param(auto: bool) -> list[str]:
@@ -34,12 +20,12 @@ def check_auto_param(auto: bool) -> list[str]:
                 Default is False.
     :return: List of URLs
     """
-    logging.info('Checking if the user has parsed \'auto\' parameter as True...')
+    logger.info('Checking if the user has parsed \'auto\' parameter as True...')
     if auto is True:
-        logging.info(f'{auto = }. Automatically get URLs')
+        logger.info(f'{auto = }. Automatically get URLs')
         return get_urls_auto.get_urls_auto()
     elif auto is False:
-        logging.info(f'{auto = }. User manually entered URLs')
+        logger.info(f'{auto = }. User manually entered URLs')
 
 
 def main(auto: bool = False, urls: list[str] = None) -> None:
@@ -50,25 +36,25 @@ def main(auto: bool = False, urls: list[str] = None) -> None:
                 Default is False.
     :return: None
     """
-    logging.info('Starting main function...')
+    logger.info('Starting main function...')
     user_input_list = []
     if auto is True:
         user_input_list: list[str] = check_auto_param(auto)
     elif auto is False:
         user_input_list = urls
-    logging.debug(f'{user_input_list = }')
+    logger.debug(f'{user_input_list = }')
 
     try:
         for url in user_input_list:
             character_name: str = web_scrap.extract_char_name(url)
-            logging.debug(f'{character_name = }')
+            logger.debug(f'{character_name = }')
 
             first_output_path = f"hsr/{character_name}.xlsx"
             second_output_path = f"hsr/hsr_updated/{character_name}.xlsx"
 
             web_scrap.scrape(url, character_name, first_output_path, second_output_path)
     except Exception as e:
-        logging.error(f'Error: {e}')
+        logger.error(f'Error: {e}')
 
 
 if __name__ == '__main__':
