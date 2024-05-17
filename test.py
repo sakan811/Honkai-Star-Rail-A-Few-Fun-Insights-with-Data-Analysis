d3a70651d5ccc13df5c15fdd1406da63ecbc8ef3
Sakan <sakanbeer@gmail.com>
2024-05-18 03:01:03 +0700

    Update v2.0.0 - Added ThreadPoolExecutor for hsr_scrape method inside HonkaiStarRailScrapeStats class - Adjusted test.py - Added method to find duplicate Level column within the same character to SQLitePipeline class

--- test.py
+++ test.py
@@ -16,35 +16,35 @@ def test_hsr_scrape_manual():
     database = 'hsr.db'
 
     with sqlite3.connect(database) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(f"SELECT COUNT(*) FROM Stats")
-            result = cursor.fetchall()  # Fetch all results
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT COUNT(*) FROM Stats")
+        result = cursor.fetchall()  # Fetch all results
 
-            # Assert that there should be records
-            assert len(result) > 0
+        # Assert that there should be records
+        assert len(result) > 0
 
     with sqlite3.connect(database) as connection:
-        with connection.cursor() as cursor:
-            query = """
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            Character,
+            Level
+        FROM (
             SELECT
                 Character,
-                Level
-            FROM (
-                SELECT
-                    Character,
-                    Level,
-                    ROW_NUMBER() OVER (PARTITION BY Character, Level ORDER BY Level) AS RowNum
-                FROM
-                    Stats
-            ) subquery
-            WHERE
-                RowNum > 1;
-            """
-            cursor.execute(query)
-            result = cursor.fetchall()  # Fetch all results
+                Level,
+                ROW_NUMBER() OVER (PARTITION BY Character, Level ORDER BY Level) AS RowNum
+            FROM
+                Stats
+        ) subquery
+        WHERE
+            RowNum > 1;
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()  # Fetch all results
 
-            # Assert that there should be no duplicates in Level for each Character
-            assert len(result) == 0
+        # Assert that there should be no duplicates in Level for each Character
+        assert len(result) == 0
 
 
 def test_full_process():
@@ -59,43 +59,43 @@ def test_full_process():
     database = 'hsr.db'
 
     with sqlite3.connect(database) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(f"SELECT COUNT(*) FROM Stats")
-            result = cursor.fetchall()  # Fetch all results
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT COUNT(*) FROM Stats")
+        result = cursor.fetchall()  # Fetch all results
 
-            # Assert that there should be records
-            assert len(result) > 0
-
-    with sqlite3.connect(database) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(f"SELECT COUNT(*) FROM Characters")
-            result = cursor.fetchall()  # Fetch all results
-
-            # Assert that there should be records
-            assert len(result) > 0
+        # Assert that there should be records
+        assert len(result) > 0
 
     with sqlite3.connect(database) as connection:
-        with connection.cursor() as cursor:
-            query = """
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT COUNT(*) FROM Characters")
+        result = cursor.fetchall()  # Fetch all results
+
+        # Assert that there should be records
+        assert len(result) > 0
+
+    with sqlite3.connect(database) as connection:
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            Character,
+            Level
+        FROM (
             SELECT
                 Character,
-                Level
-            FROM (
-                SELECT
-                    Character,
-                    Level,
-                    ROW_NUMBER() OVER (PARTITION BY Character, Level ORDER BY Level) AS RowNum
-                FROM
-                    Stats
-            ) subquery
-            WHERE
-                RowNum > 1;
-            """
-            cursor.execute(query)
-            result = cursor.fetchall()  # Fetch all results
+                Level,
+                ROW_NUMBER() OVER (PARTITION BY Character, Level ORDER BY Level) AS RowNum
+            FROM
+                Stats
+        ) subquery
+        WHERE
+            RowNum > 1;
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()  # Fetch all results
 
-            # Assert that there should be no duplicates in Level for each Character
-            assert len(result) == 0
+        # Assert that there should be no duplicates in Level for each Character
+        assert len(result) == 0
 
 
 if __name__ == '__main__':
