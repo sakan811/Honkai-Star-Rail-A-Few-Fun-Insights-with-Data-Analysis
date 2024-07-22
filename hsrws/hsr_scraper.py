fe0d924c638fac5c1535f1998a16ce3108382d57
Sakan <sakanbeer@gmail.com>
2024-07-22 23:01:32 +0700

    v4.1.0 - Adjusted headers for POST request

--- hsrws/hsr_scraper.py
+++ hsrws/hsr_scraper.py
@@ -44,16 +44,8 @@ def get_headers() -> dict:
         'Content-Type': 'application/json;charset=UTF-8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
         'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'Accept-Language': 'th-TH,th;q=0.9,en;q=0.8,ja;q=0.7',
         'Origin': 'https://wiki.hoyolab.com',
         'Referer': 'https://wiki.hoyolab.com/',
-        'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        'Sec-Ch-Ua-Mobile': '?0',
-        'Sec-Ch-Ua-Platform': '"Windows"',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
         'X-Rpc-Language': 'en-us',
         'X-Rpc-Wiki_app': 'hsr'
     }
