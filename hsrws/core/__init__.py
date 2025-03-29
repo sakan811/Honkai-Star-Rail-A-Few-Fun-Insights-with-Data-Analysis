da6af193cced5a148c68fd3ecdd8c34117f3e37e
Sakan Nirattisaykul <sakanbeer@gmail.com>
2025-03-29 17:39:54 +0700

    refactor: reorganize imports in core module for clarity

--- hsrws/core/__init__.py
+++ hsrws/core/__init__.py
@@ -1,5 +1,6 @@
 """Core functionality for HSR web scraper."""
 
-from hsrws.core.scraper import Scraper, get_headers, get_payload
+from hsrws.core.scraper import Scraper
+from hsrws.utils.payload import get_headers, get_payload
 
 __all__ = ["Scraper", "get_headers", "get_payload"]
